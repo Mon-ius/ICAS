9c6f99296412b49823341d5d0e11dab561e69947
monius <zy421741276@hotmail.com>
2017-06-28 23:39:29 +0800

    init

---
+++ cms/local_settings.py
@@ -0,0 +1,49 @@
+# This file is exec'd from settings.py, so it has access to and can
+# modify all the variables in settings.py.
+
+DEBUG = False
+
+# Make these unique, and don't share it with anybody.
+SECRET_KEY = "#xvb-r#j&+j(bttz#85o5sc3^y_+)*@151psg&1+sg(bj49v4@"
+NEVERCACHE_KEY = "y(j0iz@g0np6cu9u5k&#5=(^7=8fz(f(5v_gih6@v+v-2u+ve*"
+
+DATABASES = {
+    "default": {
+        # Ends with "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
+        "ENGINE": "django.db.backends.sqlite3",
+        # DB name or path to database file if using sqlite3.
+        "NAME": "dev.db",
+        # Not used with sqlite3.
+        "USER": "",
+        # Not used with sqlite3.
+        "PASSWORD": "",
+        # Set to empty string for localhost. Not used with sqlite3.
+        "HOST": "",
+        # Set to empty string for default. Not used with sqlite3.
+        "PORT": "",
+    }
+}
+
+###################
+# DEPLOY SETTINGS #
+###################
+
+# Domains for public site
+# ALLOWED_HOSTS = [""]
+
+# These settings are used by the default fabfile.py provided.
+# Check fabfile.py for defaults.
+
+# FABRIC = {
+#     "DEPLOY_TOOL": "rsync",  # Deploy with "git", "hg", or "rsync"
+#     "SSH_USER": "",  # VPS SSH username
+#     "HOSTS": [""],  # The IP address of your VPS
+#     "DOMAINS": ALLOWED_HOSTS,  # Edit domains in ALLOWED_HOSTS
+#     "REQUIREMENTS_PATH": "requirements.txt",  # Project's pip requirements
+#     "LOCALE": "en_US.UTF-8",  # Should end with ".UTF-8"
+#     "DB_PASS": "",  # Live database password
+#     "ADMIN_PASS": "",  # Live admin user password
+#     "SECRET_KEY": SECRET_KEY,
+#     "NEVERCACHE_KEY": NEVERCACHE_KEY,
+# }
+SITE_TITLE = "ICAS"
---
+++ cms_test/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CmsTestConfig(AppConfig):
+    name = 'cms_test'
